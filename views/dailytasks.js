fcebcb639aa6358eb67874fb64fe44d7068510c5
unknown <dylansawrie@gmail.com>
2025-06-11 19:23:17 -0500

    week 6 homework fixed

--- views/dailytasks.js
+++ views/dailytasks.js
@@ -1,9 +1,7 @@
 import html from "html-literal";
 
 export default () => html`
-  <header class="archivo-black-regular">Daily Tasks</header>
-
-  <h1 class="archivo-black-regular"><a href="/home" data-navigo>Return To Home Page</a></h1>
+   <h1 class="archivo-black-regular"><a href="/home" data-navigo>Return To Home Page</a></h1>
   <form><label><input type="checkbox"><input type="text"></label></form>
   <form><label><input type="checkbox"><input type="text"></label></form>
   <form><label><input type="checkbox"><input type="text"></label></form>
