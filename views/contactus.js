ff9ddc3f4a371fd4384ba32d8bc26e2fc7d314bf
unknown <dylansawrie@gmail.com>
2025-07-02 18:16:22 -0500

    remove phone number

--- views/contactus.js
+++ views/contactus.js
@@ -3,6 +3,5 @@ import html from "html-literal";
 export default () => html`
 
 <h1 class="archivo-black-regular"><a href="/home" data-navigo>Return To Home Page</a></h1>
-  <h2 class="archivo-black-regular">Phone: 714-442-5122</h2>
   <h2 class="archivo-black-regular">E-mail: dylansawrie@gmail.com</h2>
 `;
